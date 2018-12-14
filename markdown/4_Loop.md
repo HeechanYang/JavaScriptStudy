862a3eabcd6cbd0d3b11affe58a2692496be6a93
HeechanYang <yhc944@gmail.com>
2018-12-15 01:43:14 +0900

    [ADD] Loop

---
+++ markdown/4_Loop.md
@@ -0,0 +1,18 @@
+# JavaScript의 반복문
+
+for
+
+while
+
+do-while
+
+Array.foreach()
+
+
+break;  
+
+continue;
+
+
+# 참고 문헌
+- [https://developer.mozilla.org/ko/docs/Learn/JavaScript/Building_blocks/Looping_code](https://developer.mozilla.org/ko/docs/Learn/JavaScript/Building_blocks/Looping_code)
--- src/3_DataType.js
+++ src/3_DataType.js
@@ -1,27 +1,34 @@
 var a;
-console.log(typeof(a)); // undefined
+console.log(typeof (a)); // undefined
 a = 10;
-console.log(typeof(a)); // number
+console.log(typeof (a)); // number
 a = "txt";
-console.log(typeof(a)); // string
+console.log(typeof (a)); // string
 a = false;
-console.log(typeof(a)); // boolean
+console.log(typeof (a)); // boolean
 a = null;
-console.log(typeof(a)); // object???
+console.log(typeof (a)); // object???
+// console.log('a instanceof null : ' + (a instanceof null)); // Date
+a = [1, 2, 3, 4];
+console.log(typeof (a)); // object???
+console.log('a instanceof Array : ' + (a instanceof Array)); // Date
 
-a = function(){}
-console.log(typeof(a)); // function
+a = function () { };
+console.log(typeof (a)); // function
+a = new Date();
+console.log(typeof (a)); // object???
+console.log('a instanceof Date : ' + (a instanceof Date)); // Date
 
 
 
 // "String Object" vs "String Literal"
 s = "string1"
 console.log()
-console.log('typeof("string1") : ' + (typeof("string1")))
-console.log('typeof("string1") == "string" : ' + (typeof("string1") == "string"));
+console.log('typeof("string1") : ' + (typeof ("string1")))
+console.log('typeof("string1") === "string" : ' + (typeof ("string1") === "string"));
 console.log('"string1" instanceof String : ' + ("string1" instanceof String));
 console.log()
-console.log('typeof(new String("string1")) : ' + (typeof(new String("string1"))))
-console.log('typeof(new String("string1")) == "object" : ' + (typeof(new String("string1")) == "object"));
-console.log('typeof(new String("string1")) == string : ' + (typeof(new String("string1")) == "string"));
+console.log('typeof(new String("string1")) : ' + (typeof (new String("string1"))))
+console.log('typeof(new String("string1")) === "object" : ' + (typeof (new String("string1")) === "object"));
+console.log('typeof(new String("string1")) === string : ' + (typeof (new String("string1")) === "string"));
 console.log('new String("string1") instanceof String : ' + (new String("string1") instanceof String));
